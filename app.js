0e59a938c0200097b30dae622e3afb06137b16b0
Phak <psuram2@uic.edu>
2020-06-02 22:56:32 -0500

    Passport

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');  //session
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -55,28 +57,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //check user for authentication before displaying static info
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-if(!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
-}
-else {
-  if (req.session.user === 'authenticated') {
-    next();
+    next(err);
   }
   else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
+        next();
   }
-}
 }
 
 app.use(auth)
---
+++ authenticate.js
@@ -0,0 +1,9 @@
+//using this file to store authentication strategies
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
